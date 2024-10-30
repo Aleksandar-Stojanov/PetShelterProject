51d98e9e1e94166d67cd3e30fc45dd9369777848
Aleksandar-Stojanov <107862975+Aleksandar-Stojanov@users.noreply.github.com>
2024-10-30 21:59:15 +0100

    Update Medical_records_Repository.java

--- src/main/java/com/example/demo/repository/Medical_records_Repository.java
+++ src/main/java/com/example/demo/repository/Medical_records_Repository.java
@@ -1,7 +1,7 @@
-package com.example.demo.repository;
+// package com.example.demo.repository;
 
-import com.example.demo.model.Medical_records;
-import org.springframework.data.jpa.repository.JpaRepository;
+// import com.example.demo.model.Medical_records;
+// import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface Medical_records_Repository extends JpaRepository<Medical_records,Integer> {
-}
+// public interface Medical_records_Repository extends JpaRepository<Medical_records,Integer> {
+// }
