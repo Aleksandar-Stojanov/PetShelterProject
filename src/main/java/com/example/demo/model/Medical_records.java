6b52ec40fe20c185aa8f822f2a5ceb41e5460968
Aleksandar-Stojanov <107862975+Aleksandar-Stojanov@users.noreply.github.com>
2024-10-30 21:58:49 +0100

    Update Medical_records.java

--- src/main/java/com/example/demo/model/Medical_records.java
+++ src/main/java/com/example/demo/model/Medical_records.java
@@ -1,39 +1,39 @@
-package com.example.demo.model;
+// package com.example.demo.model;
 
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
+// import jakarta.persistence.*;
+// import lombok.AllArgsConstructor;
+// import lombok.NoArgsConstructor;
 
-import java.util.Date;
+// import java.util.Date;
 
-@Entity
-@AllArgsConstructor
-@NoArgsConstructor
-@Table(name = "medical_records")
-public class Medical_records {
+// @Entity
+// @AllArgsConstructor
+// @NoArgsConstructor
+// @Table(name = "medical_records")
+// public class Medical_records {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id_records;
+//     @Id
+//     @GeneratedValue(strategy = GenerationType.IDENTITY)
+//     private int id_records;
 
-    @Column(length = 100)
-    private String medical_condition;
+//     @Column(length = 100)
+//     private String medical_condition;
 
-    @Column(name = "date_of_examination", nullable = false)
-    private Date date_of_examination;
+//     @Column(name = "date_of_examination", nullable = false)
+//     private Date date_of_examination;
 
-    @Column(name = "vaccinations", length = 100, nullable = false)
-    private String vaccinations;
+//     @Column(name = "vaccinations", length = 100, nullable = false)
+//     private String vaccinations;
 
-    @OneToOne
-    @JoinColumn(name = "id_pet", nullable = false, unique = true)
-    private Pet pet;
+//     @OneToOne
+//     @JoinColumn(name = "id_pet", nullable = false, unique = true)
+//     private Pet pet;
 
 
-    public Medical_records(String medicalCondition, Date dateOfExamination, String vaccinations, Pet pet) {
-        this.medical_condition = medicalCondition;
-        this.date_of_examination = dateOfExamination;
-        this.vaccinations = vaccinations;
-        this.pet = pet;
-    }
-}
+//     public Medical_records(String medicalCondition, Date dateOfExamination, String vaccinations, Pet pet) {
+//         this.medical_condition = medicalCondition;
+//         this.date_of_examination = dateOfExamination;
+//         this.vaccinations = vaccinations;
+//         this.pet = pet;
+//     }
+// }
